3c42af7b7d7c0c5fea9e4498635b22157d52930c
FuSan21 <35169687+FuSan21@users.noreply.github.com>
2024-01-16 13:15:00 +0600

    Add Web3 integration and voting functionality

--- index.js
+++ index.js
@@ -12,8 +12,36 @@ app.get('/css/login.css', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/css/login.css'));
 });
 
+app.get('/css/index.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/css/index.css'));
+});
+
+app.get('/css/admin.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/css/admin.css'));
+});
+
 app.get('/js/login.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/js/login.js'));
+});
+
+app.get('/js/app.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/js/app.js'));
+});
+
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/html/index.html'));
+});
+
+app.get('/dist/app.bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/dist/app.bundle.js'));
+});
+
+app.get('/assets/eth5.jpg', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/assets/eth5.jpg'));
+});
+
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/favicon.ico'));
 });
 
 // Start the server
